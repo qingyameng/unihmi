5b9c4d45ae68b7d153ac0b11286c51b315b5716d
青崖梦 <1129514277@qq.com>
2021-07-21 12:22:41 +0800

    add

--- caches/configs/database.php
+++ caches/configs/database.php
@@ -16,4 +16,20 @@ return array (
 		),
 );
 
+// return array (
+// 	'default' => array (
+// 		'hostname' => 'localhost',
+// 		'port' => 3306,
+// 		'database' => 'bxtouch-php',
+// 		'username' => 'bxtouch-php',
+// 		'password' => '8bNsx78hwKSGPd2A',
+// 		'tablepre' => 'u_',
+// 		'charset' => 'utf8',
+// 		'type' => 'mysql',
+// 		'debug' => true,
+// 		'pconnect' => 0,
+// 		'autoconnect' => 0
+// 		),
+// );
+
 ?>
